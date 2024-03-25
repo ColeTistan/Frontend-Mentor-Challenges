794acba996b9e73983b01079bccc0dd0151db2eb
coletistan <coletistan@gmail.com>
2024-03-25 18:10:07 -0400

    Added mobile responsive design for nav list.

---
+++ news-homepage-main/app.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu");
+const navList = document.querySelector(".nav-list");
+
+// TODO - add functionality to open and close nav list
+// hamburgerMenu.addEventListener("click", () => {
+
+// });
