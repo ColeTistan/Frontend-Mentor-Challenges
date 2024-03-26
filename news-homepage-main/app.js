7f55aed19f3136a04623ae3bbffeb65743839109
coletistan <coletistan@gmail.com>
2024-03-26 11:43:06 -0400

    added JS functionality to open and close sidebar in responsive design.

--- news-homepage-main/app.js
+++ news-homepage-main/app.js
@@ -4,6 +4,18 @@ const closeMenu = document.querySelector(".close-menu");
 const navList = document.querySelector(".nav-list");
 
 // TODO - add functionality to open and close nav list
-// hamburgerMenu.addEventListener("click", () => {
+hamburgerMenu.addEventListener("click", () => {
+  if (navList.classList.contains("hide")) {
+    navList.classList.remove("hide");
+    navList.classList.add("show");
+    body.style.background = "rgba(0,0,0,0.5)";
+  }
+});
 
-// });
+closeMenu.addEventListener("click", () => {
+  if (navList.classList.contains("show")) {
+    navList.classList.remove("show");
+    navList.classList.add("hide");
+    body.style.background = "rgba(0,0,0,0)";
+  }
+});
