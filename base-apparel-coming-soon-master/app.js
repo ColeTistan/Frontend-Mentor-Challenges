3c5536753f461915fb32d65ca207f2f158ed2c8c
coletistan <coletistan@gmail.com>
2024-03-28 12:28:12 -0400

    Added desktop responsive design for base apparel.

--- base-apparel-coming-soon-master/app.js
+++ base-apparel-coming-soon-master/app.js
@@ -3,15 +3,19 @@ const submitButton = document.querySelector("button");
 const errorIcon = document.querySelector(".error-icon");
 const errorMessage = document.querySelector(".error-message");
 
-const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const emailRegex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 submitButton.addEventListener("click", () => {
-    let emailInputText = emailInput.value;
-    if (!emailInputText.match(emailRegex)) {
-        emailInput.style.border = "1px solid rgb(249, 98, 98)";
-        errorIcon.style.display = "block";
-        errorMessage.style.display = "flex";
-    } else {
-        alert("Successfully signed up!")
-    }
+  let emailInputText = emailInput.value;
+  if (!emailInputText.match(emailRegex)) {
+    emailInput.style.border = "1px solid rgb(249, 98, 98)";
+    errorIcon.style.display = "block";
+    errorMessage.style.display = "flex";
+  } else {
+    emailInput.style.border = "1px solid rgb(93, 242, 51)";
+    setTimeout(() => {
+      alert("Successfully signed up!");
+    }, 10);
+  }
 });
