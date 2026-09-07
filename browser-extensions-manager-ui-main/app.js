b225ad67817b1b44105a8632ce2029367b374699
coletistan <coletistan@gmail.com>
2026-09-07 11:55:50 -0400

    Added browser extension UI app from frontend mentor.

---
+++ browser-extensions-manager-ui-main/app.js
@@ -0,0 +1,36 @@
+const mainElem = document.getElementById('main');
+
+const getData = () => {
+    return fetch('data.json')
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+};
+
+// "logo": "./assets/images/logo-style-spy.svg",
+//         "name": "StyleSpy",
+//         "description": "Instantly analyze and copy CSS from any webpage element.",
+//         "isActive": true
+const renderExtensionData = (data) => {
+    mainElem.innerHTML = '';
+    data.forEach((extension, index) => {
+        const extensionElem = document.createElement('div');
+        extensionElem.classList.add('extension-item');
+        extensionElem.innerHTML = `
+            <div class="extension-content" data-id=${index}>
+            <img src="${extension.logo}" alt="${extension.name} logo">
+            <p>${extension.name}</p>
+            <p>${extension.description}</p>
+            <p>${extension.isActive ? 'Active' : 'Inactive'}</p>
+            <button class="remove">Remove</button>
+            </div>
+        `;
+        mainElem.appendChild(extensionElem);
+    });
+}
+
+getData().then(data => {
+    console.log(data);
+    renderExtensionData(data);
+});
